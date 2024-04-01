81f65e7cd3a88c1c5412ff492079b7ee38be6a44
Sangeeth-jay <sangeethj74@gmail.com>
2024-04-01 21:34:21 +0530

    fixed SL icon

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,6 +5,7 @@ import AboutNimal from './AboutNimal';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import BookNow from '../components/modals/Book';
+import SL from '../assets/reshot-icon-sri-lanka.svg';
 
 const Hero = () => {
     const headingRef = useRef(null);
@@ -50,7 +51,7 @@ const Hero = () => {
                 <h1 className='lg:hidden p-2' data-aos="zoom-in-up" data-aos-duration="1000"><span className="md:text-7xl text-6xl font-bold font-istok text-title">Nimal's</span><br /><span className='text-4xl font-bold font-istok text-title'>Home-Stay</span></h1>
                 <p className=" font-serif text-common text-xl  px-3" data-aos="zoom-in-up" data-aos-duration="1100">Ayubowan 👋 Welcome to your home away from home!<br />
                     Let's make the most of your stay in Sri Lanka! 🇱🇰🐘🌴🐢🥥🏵</p>
-                <img src="src\assets\reshot-icon-sri-lanka.svg" alt="" className='w-10' data-aos="zoom-in-up" data-aos-duration="1100" />
+                <img src={SL} alt="" className='w-10' data-aos="zoom-in-up" data-aos-duration="1100" />
                 <br />
                 <button data-aos="zoom-in-up" data-aos-duration="1200" onClick={() => setOpen(true)} className="font-istok  text-common  relative px-3 py-1 rounded-md bg-transparent isolation-auto border-[3px]  before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-left-full before:hover:left-0 before:rounded-full before:bg-yellow-400 before:-z-10 before:aspect-square before:hover:scale-150 overflow-hidden before:hover:duration-700">
                     Book Now
