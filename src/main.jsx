34dc59723da1242408897ead58b461c300da550e
Sangeeth-jay <sangeethj74@gmail.com>
2024-04-23 09:48:04 +0530

    add google analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import ReactGA from 'react-ga4'
+
+ReactGA.initialize('G-FW7MWPPJF0');
+ReactGA.send({
+  hitType: 'pageview',
+  page: window.location.pathname + window.location.search,
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
